bfeee5c70ec4ad2c0fec05a69b698cde6eea551a
seanmarchand <smarcha6@uwo.ca>
2019-03-29 17:09:57 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,6 +1,58 @@
 public class Square {
 	private Unit unitOnHere;
+	// Blocked would be used if an ability puts like spikes on a square so you cantpublic class Square {
+	private Unit unitOnHere;
 	// Blocked would be used if an ability puts like spikes on a square so you cant
+	// move to it
+	private boolean blocked;
+
+	public Square(Unit un) {
+		unitOnHere = un;
+		blocked = false;
+	}
+
+	public void attacked(int damage, int armourValue, double accuracy) {
+		if (!unitOnHere.testMiss(accuracy, 0)) {
+			// Give testMiss the unit and any modifiers a specific attack has
+			// Give test dodge only the unit
+			if (!unitOnHere.testDodge()) {
+				unitOnHere.damageTaken(damage, armourValue);
+				if (unitOnHere.getHealth(armourValue) <= 0) {
+					removeUnit(armourValue);
+				}
+			} else {
+				// move the unit to where it can be moved
+				System.out.println("A dodge happened");
+			}
+		} else {
+			// The enemy missed, have it target a different square
+			System.out.println("A miss happened");
+		}
+	}
+	public void stunned() {
+		unitOnHere.stunned=true;
+	}
+	public void blind() {
+		unitOnHere.accuracyValue-=0.2;
+	}
+	public Unit selectSquare() {
+		return unitOnHere;
+	}
+
+	public void hover() {
+		// Highlight the square that is hovered
+	}
+
+	public void removeUnit(int armourValue) {
+		unitOnHere.somethingDied(armourValue);
+		unitOnHere = null;
+	}
+
+	public void addUnit(Unit unitToAdd) {
+		unitOnHere = unitToAdd;
+	}
+}
+
 	// move to it
 	private boolean blocked;
 
