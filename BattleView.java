a020d6afd13e6895993789e55c9ff415180e623a
gilchristad <agilchr8@uwo.ca>
2019-03-29 21:28:11 -0400

    images and shit

---
+++ BattleView.java
@@ -0,0 +1,57 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.io.*;
+
+public class BattleView extends JPanel{
+
+    private static final long serialVersionUID = 1L;
+    boolean newgame = false;
+
+    public void BattleViewFrame() throws IOException {
+ 
+        JButton cs00 = new JButton();
+        JButton cs01 = new JButton();
+        JButton cs02 = new JButton();
+        JButton cs03 = new JButton();
+        JButton cs10 = new JButton();
+        JButton cs11 = new JButton();
+        JButton cs12 = new JButton();
+        JButton cs13 = new JButton();
+        JButton cs20 = new JButton();
+        JButton cs21 = new JButton();
+        JButton cs22 = new JButton();
+        JButton cs23 = new JButton();
+        JButton cs30 = new JButton();
+        JButton cs31 = new JButton();
+        JButton cs32 = new JButton();
+        JButton cs33 = new JButton();
+        JButton as00 = new JButton();
+        JButton as01 = new JButton();
+        JButton as02 = new JButton();
+        JButton as03 = new JButton();
+        JButton as10 = new JButton();
+        JButton as11 = new JButton();
+        JButton as12 = new JButton();
+        JButton as13 = new JButton();
+        JButton as20 = new JButton();
+        JButton as21 = new JButton();
+        JButton as22 = new JButton();
+        JButton as23 = new JButton();
+        JButton as30 = new JButton();
+        JButton as31 = new JButton();
+        JButton as32 = new JButton();
+        JButton as33 = new JButton();
+        JButton ability1 = new JButton();
+        JButton ability2 = new JButton();
+        JButton ability3 = new JButton();
+        JButton ability4 = new JButton();
+        JButton coward = new JButton();
+
+
+
+    }
+}
---
+++ RestArea.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.io.*;
+
+public class RestArea extends JPanel{
+
+    private static final long serialVersionUID = 1L;
+
+    public void RestAreaFrame() throws IOException {
+    
+        ImageIcon background = new ImageIcon("Images/restarea.jpg");
+        JButton restb = new JButton();
+        JButton trainb = new JButton();
+
+    }
+}
